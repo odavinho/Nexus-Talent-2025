33acbe52ac177aaab33829c00485facc541d4f75
Octávio Martins <odavinho@gmail.com>
2025-10-17 22:34:21 +0000

    Try fixing this error: `Runtime Error: Error: FormDescription is not def

--- src/app/dashboard/recruiter/company-profile/page.tsx
+++ src/app/dashboard/recruiter/company-profile/page.tsx
@@ -5,7 +5,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from "@/components/ui/input";
 import { Textarea } from '@/components/ui/textarea';
 import { ArrowLeft, Building, Save, Image as ImageIcon, PlusCircle, Trash2 } from 'lucide-react';
