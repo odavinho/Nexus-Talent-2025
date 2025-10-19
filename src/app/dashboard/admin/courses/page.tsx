e96c4f961bb498978f80080c5f556a518c671007
Octávio Martins <odavinho@gmail.com>
2025-10-19 22:52:27 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/dashboard/admin/courses/page.tsx
+++ src/app/dashboard/admin/courses/page.tsx
@@ -16,7 +16,7 @@ import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { useToast } from '@/hooks/use-toast';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
-import { GeneralReport } from '@/components/admin/general-report";
+import { GeneralReport } from "@/components/admin/general-report";
 
 
 export default function ManageCoursesPage() {
@@ -215,5 +215,3 @@ export default function ManageCoursesPage() {
     </div>
   );
 }
-
-    
